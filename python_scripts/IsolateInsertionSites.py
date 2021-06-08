d7167c3d16c0c371ec62e1c1cd8aee3f825c428c
bmorledge-hampton19 <b.morledge-hampton@wsu.edu>
2021-06-08 13:05:30 -0700

    Update IsolateInsertionSites.py
    - Output file is now sorted at the end of the script.
    - ADE2 reads are filtered out and the sequencing ID is removed from the output file for more efficient output.

--- python_scripts/IsolateInsertionSites.py
+++ python_scripts/IsolateInsertionSites.py
@@ -3,19 +3,31 @@
 from mutperiodpy.Tkinter_scripts.TkinterDialog import TkinterDialog
 from mutperiodpy.helper_scripts.UsefulFileSystemFunctions import getDataDirectory
 from typing import List
+import subprocess
 
 def isolateInsertionSites(alignedReadsFilePaths: List[str]):
 
     for alignedReadsFilePath in alignedReadsFilePaths:
 
+        print("Working with", alignedReadsFilePath)
+
         # Generate a path to the output file.
         insertionSitesFilePath = alignedReadsFilePath.rsplit('.',1)[0] + "_insertion_sites.bed"
+
+        filteredReads = 0
+        remainingReads = 0
 
         with open(alignedReadsFilePath, 'r') as alignedReadsFile:
             with open(insertionSitesFilePath, 'w') as insertionSitesFile:
 
                 for line in alignedReadsFile:
                     choppedUpLine = line.split()
+
+                    # Check for the expected ADE2 read to filter out.
+                    if choppedUpLine[0] == "chrXV" and choppedUpLine[1] == "565445" and choppedUpLine[2] == "565595" and choppedUpLine[5] == '-':
+                        filteredReads += 1
+                        continue
+                    else: remainingReads += 1
 
                     # Based on which strand the read is on, alter the position to highlight the 5' insertion site.
                     if choppedUpLine[5] == '+':
@@ -27,7 +39,14 @@ def isolateInsertionSites(alignedReadsFilePaths: List[str]):
                     else:
                         raise ValueError("Line does not have expected strand designation:\n" + line)
                     
+                    # Replace the 4th column (sequencing ID?) to conserve space.
+                    choppedUpLine[3] = '.'
+
                     insertionSitesFile.write('\t'.join(choppedUpLine) + '\n')
+
+        print("Filtered out", filteredReads, "reads at expected ADE2 Location. ", remainingReads, " reads remaining.")
+        print("Sorting...")
+        subprocess.run(("sort", insertionSitesFilePath, "-k1,1", "-k2,2n", "-k3,3n", "-o", insertionSitesFilePath), check = True)
 
 
 def main():
